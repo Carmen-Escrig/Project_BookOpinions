e0c6b51c055443480770324110d6c8437486b91d
alu0557 <aluesc0557@ieselcaminas.org>
2024-02-02 09:51:02 +0100

    Hecho follow, falta js y html

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,11 +8,50 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+
+    #[Route('/profile/{slug}/follow', name: 'user_follow')]
+    public function userFollow(ManagerRegistry $doctrine, $slug): JsonResponse
     {
-        return $this->render('user/index.html.twig', [
+        $userRepository = $doctrine->getRepository(User::class);
+        $profile = $repository->findOneBy(["slug" => $slug]);
+        $user = $userRepository->find($this->getUser()->getId());
+
+        $entityManager = $doctrine->getManager();
+
+        if(in_array($user, ($profile->getFollowers()->toArray()))) {
+            try {
+                $profile->removeFollower($user);
+                $user->removeFollowing($profile);
+
+                $entityManager->flush();
+                $result = false;
+                return new JsonResponse($result, Response::HTTP_OK);           
+            } catch (\Exception $e) {
+                return new JsonResponse($result, Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            $profile->addFollower($user);
+
+            try {
+                $entityManager->flush();
+
+                $result = true;
+                return new JsonResponse($result, Response::HTTP_OK);
+            } catch (\Exception $e) {
+                return new JsonResponse($result, Response::HTTP_INTERNAL_SERVER_ERROR);
+            }  
+        }
+    }
+
+    #[Route('/user/{slug}', name: 'user_profile')]
+    public function userProfile(): Response
+    {
+        $userRepository = $doctrine->getRepository(User::class);
+        $user = $userRepository->findOneBy(["slug" => $slug]);
+
+        return $this->render('user/profile.html.twig', [
             'controller_name' => 'UserController',
+            'user' => $user
         ]);
     }
 }
